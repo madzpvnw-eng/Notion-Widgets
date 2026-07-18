5b8cf0540747ed844e6d388b019109036a9687b2
madzpvnw-eng <madzpvnw@gmail.com>
2026-07-18 11:48:30 +0700

    Update widgets.js

--- widgets.js
+++ widgets.js
@@ -1,2 +1,17 @@
+const widgets = [
 
-const widgets = [];
+    {
+        name: "Clock",
+        icon: "🕒",
+        folder: "clock",
+        description: "Digital Clock"
+    },
+
+    {
+        name: "Calendar",
+        icon: "📅",
+        folder: "calender",
+        description: "Monthly Calendar"
+    }
+
+];

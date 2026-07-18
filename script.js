f12fb4ac62a3b6e14b6053140d0d885d2befd553
madzpvnw-eng <madzpvnw@gmail.com>
2026-07-18 11:48:45 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
+const app = document.getElementById("app");
 
-console.log("Homepage Loaded");
+app.innerHTML = `
+    <h1>Notion Widgets</h1>
+
+    <div id="grid"></div>
+`;
+
+const grid = document.getElementById("grid");
+
+widgets.forEach(widget => {
+
+    grid.innerHTML += `
+
+        <a href="./${widget.folder}/">
+
+            ${widget.icon}
+
+            <h2>${widget.name}</h2>
+
+            <p>${widget.description}</p>
+
+        </a>
+
+    `;
+
+});

daa2c93be4b64d5002a4d5018c00459d15f48663
madzpvnw-eng <madzpvnw@gmail.com>
2026-07-18 12:21:17 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
-alert("Script berhasil dimuat!");
-
 const app = document.getElementById("app");
 
-app.innerHTML = "<h1>Halo Dunia!</h1>";
+app.innerHTML = `
+    <h1>Notion Widgets</h1>
+
+    <div id="grid"></div>
+`;
+
+const grid = document.getElementById("grid");
+
+widgets.forEach(widget => {
+
+    grid.innerHTML += `
+        <a href="./${widget.folder}/">
+            <div style="font-size:32px">${widget.icon}</div>
+
+            <h2>${widget.name}</h2>
+
+            <p>${widget.description}</p>
+        </a>
+    `;
+
+});
